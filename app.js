9b951169c45d3acc655fd3a347965a2858a82aaf
hupratt <hprattdo@gmail.com>
2024-11-05 21:36:56 +0100

    push

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ const corsOption = {
 
 app.use(cors(corsOption));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(morgan('tiny'))
 app.use(helmet())
 
